a4f7221fb97d9a3ad3938438b4ac4a97cad26a18
ozysouza <ozysouzaa@gmail.com>
2025-05-30 21:33:37 -0400

    Create Category file, map for table creation and relate to activity

---
+++ src/main/java/com/desouza/event/entities/Category.java
@@ -0,0 +1,70 @@
+package com.desouza.event.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Integer id;
+    private String description;
+
+    @OneToMany(mappedBy = "category")
+    private List<Activity> activities = new ArrayList<>();
+
+    public Category() {
+    }
+
+    public Category(Integer id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Activity> getActivities() {
+        return activities;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Category category = (Category) obj;
+        return Objects.equals(id, category.id);
+    }
+
+}
